49327303e2d6cd5d9efd1e9a18542916af44bd94
Kenji Saito <ken-yo@mbr.nifty.com>
2024-10-07 23:13:23 +0900

    Bumps node modules

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,13 +4,11 @@
 )]
 
 mod cmd;
-use tauri::menu::Menu;
 
 fn main() {
   let context = tauri::generate_context!();
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![cmd::save, cmd::load])
-    .menu(Menu::os_default(&context.package_info().name))
     .run(context)
     .expect("error while running tauri application");
 }
